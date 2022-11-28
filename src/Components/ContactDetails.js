f3a04cc83fabe0b57bf2b2fa50215606d1714725
Mandavi Srivastava <mandavisrivastava10@gmail.com>
2022-11-28 19:27:08 +0530

    changes

--- src/Components/ContactDetails.js
+++ src/Components/ContactDetails.js
@@ -5,16 +5,16 @@ const ContactDetails = () => {
     <div>
       <div className="card text-dark bg-info mb-3">
         <div className="card-body">
-          <h5 className="card-title">Ashutosh Mishra</h5>
-          <p className="card-text">Address: Gorakhpur</p>
-          <h5 className="card-header">Contact Number : 905874784348</h5>
+          <h5 className="card-title">Swati Singh</h5>
+          <p className="card-text">Address: Naveliganj</p>
+          <h5 className="card-header">Contact Number : 46371143638</h5>
         </div>
       </div>
       <div className="card text-dark bg-info mb-3">
         <div className="card-body">
-          <h5 className="card-title">Satyam Mishra</h5>
-          <p className="card-text">Address: Lko</p>
-          <h5 className="card-header">Contact Number : 45745687979</h5>
+          <h5 className="card-title">Ankit Mishra</h5>
+          <p className="card-text">Address: Kanpur</p>
+          <h5 className="card-header">Contact Number : 5738261475</h5>
         </div>
       </div>
     </div>
